09b6f2ba759c8dd4f142ec0d24ac725984ed2f3d
taicancr7 <taicancr7@gmail.com>
2023-02-22 23:00:05 +0700

    update index.js

--- server/index.js
+++ server/index.js
@@ -12,13 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-
-mongoose.set("strictQuery", false);
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -32,3 +31,5 @@ mongoose
     console.log({ err });
     process.exit(1);
   });
+
+//test
